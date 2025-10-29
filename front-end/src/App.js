546e53d6897969ecfc4397eb9693f9ca2274afa5
cao-exe <tc3661@nyu.edu>
2025-10-29 02:40:10 -0400

    page created, initial ui prototype ui generated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import SignIn from './SignIn';
 import SignUp from "./SignUp";
 import CallHistory from './CallHistory';
 import TranslationLog from './TranslationLog';
+import Meeting from './Meeting';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
           <Route path="/translation-log/:meetingId" element={<TranslationLog/>} />
           {/* sign in's the first page users land on */}
           <Route path="/" element={<div className="App"><SignIn /></div>} />
+          <Route path="/meeting" element={<Meeting />} />
+          // to add: unique meeting page routes to ID
         </Routes>
       </Router>
     </>
