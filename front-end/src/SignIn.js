b8bf8b008dc7a32cf01c54a92db2cd6c3b950c60
jasmine7310 <jf4654@nyu.edu>
2025-10-28 22:30:36 -0400

    finish sign in screen

--- front-end/src/SignIn.js
+++ front-end/src/SignIn.js
@@ -1,6 +1,5 @@
 import './SignIn.css';
 import { useNavigate } from "react-router-dom";
-import './SignUp';
 
 function SignIn(){
     const navigate = useNavigate();
@@ -9,13 +8,24 @@ function SignIn(){
         navigate("/signup"); 
     };
 
+    const toHome = () => {
+        navigate("/home"); 
+    };
+
+    const checkPass = () => {
+       // need to check email and password from db!!!
+       // if no match, err message is "no account associated with email"
+       // if pass no match email, err msg is "incorrect password"
+       // ^ on that note must make err msg element & edit Sign in btn onClick checkPass
+    };
+
     return(
         <div id="signin-content">
             <p id="sub">the sign language to text converter meeting app</p>
             <img src='./shuwa.png'/>
             <p id="hook">get started by signing in</p>
             <div id="signin-form">
-                <input type='username' placeholder='email'/>
+                <input id='usernameInput' type='username' placeholder='email'/>
                 <input type='password' placeholder='password'/>
                 <button>Sign in</button>
                 <a onClick={toSignUp}>Getting started? Sign Up Here</a>
