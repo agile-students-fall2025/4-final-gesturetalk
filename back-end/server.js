2d0ec710a522ddb11162e2b187a49b55d8716463
jasmine7310 <jf4654@nyu.edu>
2025-11-08 15:08:52 -0500

    skeleton express server

---
+++ back-end/server.js
@@ -0,0 +1,82 @@
+import express from 'express'
+const app = express()
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+import multer from 'multer' 
+import axios from 'axios' 
+import dotenv from 'dotenv' 
+dotenv.config({ silent: true })
+import morgan from 'morgan'
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// Sign In
+app.get('/', (req, res) => {
+  
+})
+
+app.post('/', (req, res) => {
+
+})
+
+// Sign Up
+app.get('/signup', (req, res) => {
+  
+})
+
+app.post('/signup', (req, res) => {
+    
+})
+
+// Call History
+app.get('/call-history', (req, res) => {
+  
+})
+
+app.post('/call-history', (req, res) => {
+    
+})
+
+// Home
+app.get('/home', (req, res) => {
+  
+})
+
+app.post('/home', (req, res) => {
+    
+})
+
+// Proflile
+app.get('/profile', (req, res) => {
+  
+})
+
+app.post('/profile', (req, res) => {
+    
+})
+
+// Meeting
+// Translation log
+app.get('/meeting', (req, res) => {
+  
+})
+
+app.post('/meeting', (req, res) => {
+    
+})
+
+// Translation log
+app.get('/translation-log/:meetingId', (req, res) => {
+  
+})
+
+app.post('/translation-log/:meetingId', (req, res) => {
+    
+})
