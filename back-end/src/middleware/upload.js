a8be2e812b64cd1ccb76d5033d7a84e226a1ed3c
venetialiu <vl2289@nyu.edu>
2025-11-09 17:59:16 -0500

    upload middleware/upload.js

--- back-end/src/middleware/upload.js
+++ back-end/src/middleware/upload.js
@@ -1,1 +1,16 @@
-// TBD: upload logic for Mutler
+// TBD: pload logic for Mutler
+
+import multer from 'multer';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// multer for user upload profile image:
+const storage = multer.diskStorage({
+    // populate here, see knowledge kitchen example code
+})
+const upload = multer({ storage })
+
+export default upload;
