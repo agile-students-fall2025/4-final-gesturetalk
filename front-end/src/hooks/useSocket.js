9d5419a156361f5abdd3571370279b6daaf794b8
venetialiu <vl2289@nyu.edu>
2025-11-11 17:40:49 -0500

    update Home.js

---
+++ front-end/src/hooks/useSocket.js
@@ -0,0 +1,22 @@
+// connect to backend Socket.IO Server & define helper functions
+import { io } from "socket.io-client";
+
+const useSocket = () => {
+    // create connection to backend server
+    const socket = io("http://localhost:5000");
+
+    // define helper functions
+    const joinRoom = (roomID) => 
+
+    const sendOffer = () => 
+
+    const sendAnswer = () => 
+    
+    const sendICECandidate = () => 
+
+    const sendCaptions = () => 
+
+
+};
+
+export default useSocket;
---
+++ front-end/src/hooks/useWebRTC.js
@@ -0,0 +1,8 @@
+// handles webRTC peer connection
+
+export const useWebRTC = () => {
+    const pc = new RTCPeerConnection(rtcConfig);
+
+};
+
+export default useWebRTC;
