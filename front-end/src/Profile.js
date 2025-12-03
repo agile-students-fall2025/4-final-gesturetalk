c510a3b472df67f6277fce4edbe76b050ed23df0
Iva Park <ivapark@MacBook-Pro.lan>
2025-12-03 02:13:32 -0500

    profile mobile view fixed

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -152,7 +152,16 @@ function Profile() {
   };
 
   return (
+    
     <div id="profile-content">
+      <header>
+        <h1 className="logo1" id="logoHome1">shuwa</h1>
+        <div className="profile1" onClick={() => navigate("/profile")} style={{ cursor: "pointer" }}>
+          <img src={currentUser?.picture || "/profile.svg"} alt="Profile" />
+        </div>
+      </header>
+
+
       <svg id="gradient-blob" width="1130" height="1024" viewBox="0 0 1130 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g filter="url(#filter0_f_164_18)">
           <path fillRule="evenodd" clipRule="evenodd" d="M1042.11 519.452C1017.13 572.13 960.641 599.772 917.84 639.336C879.644 674.644 850.204 721.158 802.224 741.195C754.548 761.105 700.953 746.306 649.549 751.377C590.993 757.153 531.253 795.158 476.624 773.315C422.616 751.72 398.252 689.074 368.56 639.023C340.906 592.408 318.408 543.376 308.582 490.054C298.825 437.103 294.423 381.561 311.718 330.561C328.645 280.646 368.927 243.771 405.094 205.449C439.861 168.61 475.012 131.897 520.397 109.444C566.605 86.5831 618.02 81.6188 669.109 74.8315C727.034 67.1359 792.166 36.2312 842.015 66.7155C892.787 97.7648 879.976 179.752 916.96 226.402C955.287 274.744 1035.14 284.124 1059.38 340.875C1083.15 396.551 1068.06 464.731 1042.11 519.452Z" fill="url(#paint0_linear_164_18)"/>
@@ -170,9 +179,9 @@ function Profile() {
         </defs>
       </svg>
 
-      <p id="logo">shuwa</p>
 
       <div id="profile-card">
+        
         <button className="close-btn" id="profileCloseBtn" onClick={() => navigate("/home")}>
           ✕
         </button>
